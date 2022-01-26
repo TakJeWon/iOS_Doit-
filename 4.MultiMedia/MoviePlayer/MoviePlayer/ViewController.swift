849ac33c243d79f6b55fd16147fdba3281f0dea2
TakJeWon <taknineball@castis.com>
2022-01-26 17:05:38 +0900

    add VideoPlayer app

---
+++ 4.MultiMedia/MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 탁제원 on 2022/01/26.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playInternalMovie(_ sender: UIButton) {
+        let filePath: String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func playExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        
+        playVideo(url: url)
+    }
+    
+    func playVideo(url: NSURL){
+        
+        let playerController = AVPlayerViewController()
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true){
+            player.play()
+        }
+    }
+}
+
