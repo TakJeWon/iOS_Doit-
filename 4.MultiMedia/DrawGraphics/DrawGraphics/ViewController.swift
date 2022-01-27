b194d2159f89b6d5652d9990e0e9a3a67d725882
TakJeWon <taknineball@castis.com>
2022-01-27 14:49:13 +0900

    add DrawGraphics project

---
+++ 4.MultiMedia/DrawGraphics/DrawGraphics/ViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  DrawGraphics
+//
+//  Created by 탁제원 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func drawLine(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //Draw Line
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 70, y: 50))
+        context.addLine(to: CGPoint(x: 270, y: 250))
+        
+        context.strokePath()
+        
+        //DRaw Triangle
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 270, y: 350))
+        context.addLine(to: CGPoint(x: 70, y: 350))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func drawRectangle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //Draw Rectangle
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addRect(CGRect(x: 70, y: 100, width: 200, height: 200))
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func drawCircle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //Draw Ellipse
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 100))
+        context.strokePath()
+        
+        //Draw Circle
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 200))
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func drawArc(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //Draw Arc
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 100, y: 50))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x: 250, y: 200), radius: CGFloat(50))
+        context.addLine(to: CGPoint(x: 250, y: 200))
+        
+        context.move(to: CGPoint(x: 100, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 270, y: 250), tangent2End: CGPoint(x: 100, y: 400), radius: CGFloat(20))
+        context.addLine(to: CGPoint(x: 100, y: 400))
+        
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func drawFill(_ sender: UIButton) {
+        
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //Draw Rectangle
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.red.cgColor)
+        
+        let rectangle = CGRect(x: 70, y: 50, width: 200, height: 100)
+        context.addRect(rectangle)
+        context.fill(rectangle)
+        context.strokePath()
+        
+        //Draw Circle
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        context.setFillColor(UIColor.blue.cgColor)
+        
+        let circle = CGRect(x: 70, y: 200, width: 200, height: 100)
+        context.addEllipse(in: circle)
+        context.fillEllipse(in: circle)
+        context.strokePath()
+        
+        //Draw Triangle
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 350))
+        context.addLine(to: CGPoint(x: 270, y: 450))
+        context.addLine(to: CGPoint(x: 70, y: 450))
+        context.addLine(to: CGPoint(x: 170, y: 350))
+        context.fillPath()
+        context.strokePath()
+
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func drawFlower(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //draw circles
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 120, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 120+50, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 120-50, y: 150, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 120, y: 150-50, width: 100, height: 100))
+        context.addEllipse(in: CGRect(x: 120, y: 150+50, width: 100, height: 100))
+        
+        context.strokePath()
+        
+        //draw triangle
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 200, y: 450))
+        context.addLine(to: CGPoint(x: 140, y: 450))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.fillPath()
+        context.strokePath()
+
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+}
+
